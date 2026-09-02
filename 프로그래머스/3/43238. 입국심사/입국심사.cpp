00dce07359e16ea5fa95110e00bbb8bf7192a16c
유승지 <143983091+yseungji@users.noreply.github.com>
2026-09-02 18:38:13 +0900

    [level 3] Title: 입국심사, Time: 15.84 ms, Memory: 7.31 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    
+    long long left = 1;
+    long long right = (long long) *max_element(times.begin(), times.end()) * n;
+
+
+    while( left <= right) {
+        long long mid = (left + right) / 2;
+        long long total = 0;
+        for(int i=0; i < times.size(); i++) {
+            total += mid / times[i];
+        }
+        if(total >= n) {
+            answer = mid;
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    
+    return answer;
+}
+
