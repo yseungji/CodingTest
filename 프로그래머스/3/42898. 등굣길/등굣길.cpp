e6063f7f456f3ff5e70a1e54e3b11c546a270607
유승지 <143983091+yseungji@users.noreply.github.com>
2026-09-06 10:40:10 +0900

    [level 3] Title: 등굣길, Time: 0.07 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -7,41 +7,21 @@ using namespace std;
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
     
-    int cnt[100][100] = {0,};
-    for(int i=0;i<puddles.size();i++){
-        cnt[puddles[i][1]-1][puddles[i][0]-1] = -1;
-    }
-    cnt[0][0] = 1;
+    int cnt[101][101] = {0,};
     
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i==0 && j==0 || cnt[i][j] == -1) continue;
-            else if(i==0){
-                if(cnt[i][j-1] != -1){
-                    cnt[i][j] = cnt[i][j-1];
-                }
+    for(int i=0;i<puddles.size();i++){
+        cnt[puddles[i][1]][puddles[i][0]] = -1;
+    }
+    cnt[0][1] = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(cnt[i][j] == -1) {
+                cnt[i][j] = 0;
+                continue;
             }
-            else if(j==0){
-                if(cnt[i-1][j] != -1){
-                    cnt[i][j] = cnt[i-1][j];
-                
-                }
-            }
-            else if(cnt[i][j-1]==-1){
-                cnt[i][j] = cnt[i-1][j];
-            }
-            else if(cnt[i-1][j]==-1){
-                cnt[i][j] = cnt[i][j-1];
-                
-            }
-            else{
-                cnt[i][j] = cnt[i][j-1] + cnt[i-1][j];
-                
-            }
-            cnt[i][j] %= 1000000007;
+            cnt[i][j] = (cnt[i-1][j] + cnt[i][j-1])%1000000007;
         }
     }
-    answer = cnt[n-1][m-1]%1000000007;
-    //cout << cnt[n-1][m-1]%1000000007;
+    answer = cnt[n][m];
     return answer;
 }
